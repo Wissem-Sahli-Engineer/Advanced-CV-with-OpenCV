5b43db4a000db6fe0623d7d7245fbf11400cf4f0
Wissem Sahli <contact.ing.wissem@gmail.com>
2026-07-03 03:17:02 +0100

    utils working

--- Hand Tracking/utils.py
+++ Hand Tracking/utils.py
@@ -52,13 +52,13 @@ def get_fps(cap, pTime,type='default'):
         pTime = cTime
         return fps, pTime
 
-    elif type =="":
+    elif type =="cap":
         fps= cap.get(cv2.CAP_PROP_FPS)
         if fps<= 0:
             return 30, pTime
-        return fps
+        return fps, pTime
     else:
-        return 30.0, pTime
+        return 30, pTime
 
 
 
@@ -66,23 +66,23 @@ def main(landmarker):
     cap = cv2.VideoCapture(0)
 
     frame_count = 0
+    pTime = time.time()
 
     while True:
         test , img = cap.read()
         if not test or img is None:
             break
         
-        pTime = time.time()
         fps , pTime = get_fps(cap,pTime,"default")
         
-        timestamp_ms = int((frame_count / fps) * 1000)
+        timestamp_ms = int((frame_count / get_fps(cap,0,type='cap')[0]) * 1000)
         frame_count += 1
 
         # preprocessing
         img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
 
-        res = landmarker.detect_for_video(mp_img,timestamp_ms)
+        res = landmarker.landmarker.detect_for_video(mp_img,timestamp_ms)
 
         if res.hand_landmarks:
             for hand in res.hand_landmarks:
